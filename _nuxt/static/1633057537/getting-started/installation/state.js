330d89a93c4a927d154c27c7d2532c7e559c226c
thombruce <thombruce@users.noreply.github.com>
2021-10-27 22:41:32 +0000

    deploy: 5fb8484bba179b268ee072c32343c4d80da89a56

--- _nuxt/static/1633057537/getting-started/installation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftnt\u002F_nuxt\u002Fstatic\u002F1633057537",layout:"docs",error:a,serverRendered:true,routePath:"\u002Fgetting-started\u002Finstallation",config:{_app:{basePath:"\u002Ftnt\u002F",assetsPath:"\u002Ftnt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0c831090"}}}}(null));
